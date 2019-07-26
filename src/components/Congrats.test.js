b3b5f97ceef8bfe87ffdde5f882a8d85e2ae6d15
Aman <rumman.djuice1@gmail.com>
2019-07-26 20:37:22 +0200

    "check-props-type and setupTests.js added"

--- src/components/Congrats.test.js
+++ src/components/Congrats.test.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Enzyme,{shallow} from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-import {findByTestAttr} from '../testUtils/testUtils'
-
+//import EnzymeAdapter from 'enzyme-adapter-react-16';
+import {findByTestAttr, checkProps} from '../testUtils/testUtils'
+import checkPropTypes from 'check-prop-types'
 import Congrats from './Congrats'
 
-Enzyme.configure({adapter: new EnzymeAdapter()});
+//Enzyme.configure({adapter: new EnzymeAdapter()}); //We dont need to write these here any more because we have put them in "setupTests.js" which runs before any test
 
+const defaultProps={success:false};
 const setup=(props={})=>{
-  return shallow(<Congrats {...props}/>)
+  const setupProps={...defaultProps,...props};// If we provide any props value in the function which is similar to the defaultProps then it will just override it.
+  return shallow(<Congrats {...setupProps}/>)
 };
 
+
 test('render without error',()=>{
-    const wrapper=setup();
+    const wrapper=setup({success:false});
 
     const CongratsComp=findByTestAttr(wrapper, "component-congrats");
     expect(CongratsComp.length).toBe(1);
@@ -30,4 +33,10 @@ test('render non-empty congrats when `success` prop is true',()=>{
 
     const CongratsComp=wrapper.find("[data-test='component-congrats']");
     expect(CongratsComp.text().length).not.toBe(0);
+});
+
+test('does not throw warning with with expected props', ()=>{
+    //this function is going to return the error instead of just console.log it
+    const propError=checkPropTypes(Congrats.propTypes, {success:false}, 'prop', Congrats.name)// 1st the Component with propType object, 2nd the prop that we want to check, 3rd declare the 2nd param as a 'prop', 4th the name of the Component.
+    expect(propError).toBeUndefined();// if the prop is a boolean then it should not throw any error then the error will be undefined
 });
--- src/testUtils/testUtils.js
+++ src/testUtils/testUtils.js
@@ -1,3 +1,10 @@
+import checkPropTypes from 'check-prop-types'
+
 export const findByTestAttr=(wrapper, val)=>{
     return wrapper.find(`[data-test='${val}']`)// to find certain element using a attribute value.
+};
+
+export const checkProps=(Component, props)=>{
+    const propError=checkPropTypes(Component.propTypes, {...props}, 'props', Component.name);
+    expect(propError).teBeUndefined();
 };

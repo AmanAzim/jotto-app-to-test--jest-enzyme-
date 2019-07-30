718f29ca1821f65fbc64b772865cf33a7c3fac51
Aman <rumman.djuice1@gmail.com>
2019-07-30 19:31:17 +0200

    "testing redux access for App.js"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; //for bootstrap
+import {connect} from 'react-redux'
 
 import Congrats from './components/Congrats'
 import GuessedWords from './components/GuessedWords'
 import Input from './components/Input'
+import {guessWord, getSecretWord} from "./store/actions/actionCreator";
+import secretWordReducer from "./store/reducers/secretWordReducer";
 
-function App() {
+
+
+
+const App=(props)=>{
+  const {success, guessedWord, secretWord, getSecretWord}=props;
 
   return (
       <div className="container">
           <h1>Jotto</h1>
-          <Congrats success={true}/>
-          <GuessedWords guessedWords={[{guessedWord:'train', letterMatchCount:3 }]}/>
+          <Congrats success={success}/>
+          <GuessedWords guessedWords={guessedWord}/>
       </div>
   );
 }
 
-export default App;
+const mapStateToProps=(state)=>{
+    return {
+        success:state.successReducer,
+        guessedWord:state.guessWordReducer,
+        secretWord:state.secretWordReducer
+    }
+};
+const mapDispatchToProps=(dispatch)=>{
+    return {
+        getSecretWord:()=>dispatch(getSecretWord())
+    }
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {applyMiddleware, createStore} from "redux";
+import {rootReducer} from "./index";
+import ReduxThunk from "redux-thunk";
+//import {shallow} from "enzyme";
+import Enzyme,{shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+import App from './App';
+import secretWordReducer from "./store/reducers/secretWordReducer";
+import guessWordReducer from "./store/reducers/guessWordReducer";
+
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+const setup=(initialState={})=>{
+
+    const store=createStore(rootReducer, initialState, applyMiddleware(ReduxThunk));
+
+    const wrapper=shallow(<App store={store}/>).dive();// only one dive() because we just want to see access the components properties not childrens
+
+    return wrapper
+};
+
+describe('redux properties', ()=>{
+     test('has access to `success` state', ()=>{
+        const successReducer=false;
+        const wrapper=setup({successReducer});
+        const successState=wrapper.props().success;// This way of accessing props is for functional component and for class based component >wrapper.instance().props.guessWord;
+        expect(successState).toBe(false)
+     });
+     test('has access to `secretWord` state', ()=>{
+        const secretWordReducer='party';
+        const wrapper=setup({secretWordReducer});
+        const secretWordProp=wrapper.props().secretWord;// This way of accessing props is for functional component and for class based component >wrapper.instance().props.guessWord;
+        expect(secretWordProp).toBe(secretWordReducer);
+     });
+     test('has access to `guessedWord` state', ()=>{
+        const guessWordReducer=[{guessedWord:'train', letterMatchCount:3 }];
+        const wrapper=setup({guessWordReducer});
+        const guessedWordProp=wrapper.props().guessedWord;// This way of accessing props is for functional component and for class based component >wrapper.instance().props.guessWord;
+        expect(guessedWordProp).toBe(guessWordReducer);
+     });
+     test('has access to `getSecretWord` function in props', ()=>{
+        const wrapper=setup();
+        const getSecretWord=wrapper.props().getSecretWord;// This way of accessing props is for functional component and for class based component >wrapper.instance().props.guessWord;
+        expect(getSecretWord).toBeInstanceOf(Function);
+     });
 });
